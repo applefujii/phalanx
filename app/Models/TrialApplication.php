0058a6c1032767d6de2353bda49c21dd32843e17
望月 <fmochizukif@gmail.com>
2021-10-25 13:35:35 +0900

    体験・見学申込テーブルのモデル作成 マイグレーションとシーダーにコメント追加

---
+++ app/Models/TrialApplication.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * 体験・見学申込テーブルのモデル
+ * 
+ * @author Fumio Mochizuki
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TrialApplication extends Model
+{
+    use HasFactory;
+
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'trial_applications';
+
+    /**
+     * モデルのタイムスタンプを更新するかの指示
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * 関連する事業所マスタの内容を取得
+     */
+    public function office()
+    {
+        return $this->belongsTo('App\Office');
+    }
+}
