79c9d4a889e576e9b64e48c498ecfcf94c12b250
k.k.apple0430 <k.k.apple0430@gmail.com>
2021-10-25 11:42:33 +0900

    チャットルームテーブルのmigrationファイルを作成しました(60%)

---
+++ database/migrations/2021_10_25_112019_create_chat_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string("room_title", "100");
+            $table->foreignId("office_id");
+            $table->foreignId("user_id")->nullable()->default(null);
+            $table->foreignId("create_user_id");
+            $table->foreignId("update_user_id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_rooms');
+    }
+}
