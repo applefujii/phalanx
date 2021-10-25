62f2aba0356d63cc422dd82671789726d52af8d0
望月 <fmochizukif@gmail.com>
2021-10-25 13:26:09 +0900

    事業所マスタテーブルと体験・見学申込テーブルのmigration作成 事業所マスタのseeder作成

---
+++ database/seeders/OfficesSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class OfficesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("offices")->insert([
+            [
+                "office_name" => "アップル梅田",
+                "sort" => 10,
+                "create_user_id" => 1,
+                "update_user_id" => 1,
+                "updated_at" => "2021-04-01 12:34:56",
+                "created_at" => "2021-04-01 12:34:56",
+            ],
+            [
+                "office_name" => "ミント大阪",
+                "sort" => 20,
+                "create_user_id" => 1,
+                "update_user_id" => 1,
+                "updated_at" => "2021-04-01 12:34:56",
+                "created_at" => "2021-04-01 12:34:56",
+            ],
+            [
+                "office_name" => "メープル関西",
+                "sort" => 30,
+                "create_user_id" => 1,
+                "update_user_id" => 1,
+                "updated_at" => "2021-04-01 12:34:56",
+                "created_at" => "2021-04-01 12:34:56",
+            ],
+        ]);
+    }
+}
