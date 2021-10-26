805fc7355b77720dc303f7dd968d61058288987f
k.k.apple0430 <k.k.apple0430@gmail.com>
2021-10-26 11:16:41 +0900

    chat_textsテーブルのモデルの作成(100%)

--- phalanx/app/Models/Chat_text.php
+++ phalanx/app/Models/Chat_text.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * チャットテキストテーブルのモデル
+ * 
+ * @author Kakeru Kusada
+ */
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +14,32 @@ use Illuminate\Database\Eloquent\Model;
 class Chat_text extends Model
 {
     use HasFactory;
+
+    /**
+     * モデルと関連しているテーブル
+     * 
+     * @var string
+     */
+    protected $table = "chat_texts";
+
+    /**
+     * タイムスタンプを更新するかどうかの指示
+     * 
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * 関連するチャットルームのデータの取得
+     */
+    public function chat_rooms() {
+        return $this->belongsTo(Chat_room::class);
+    }
+
+    /**
+     * 関連するユーザーマスタのデータの取得
+     */
+    public function users() {
+        return $this->belongsTo(User::class);
+    }
 }
