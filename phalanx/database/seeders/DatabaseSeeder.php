6a76c8439e092c58993636b1962db82ed40a603c
apple-fujii <apple.fujii333@gmail.com>
2021-10-29 11:43:55 +0900

    人選択モーダル、APIからデータを読む部分以外が完成。 通知用のテーブル定義と関連した各種ファイルを作成。

--- phalanx/database/seeders/DatabaseSeeder.php
+++ phalanx/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(OfficesSeeder::class);// 事業所マスタ
+        $this->call(NotificationsSeeder::class);// 通知テーブル
         \App\Models\TrialApplication::factory(50)->create();
     }
 }
