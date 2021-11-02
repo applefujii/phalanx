3b0fcf79745643cb6d6518010db9c50567bd9766
kawashima.apple.umeda@gmail.com <kawashima>
2021-11-02 14:09:32 +0900

    スタイルをCSSに分離させた

--- resources/views/user_master_index.blade.php
+++ resources/views/user_master_index.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.app')
-@section('content')
 
-<!--=============
-    先に見た目だけ
-===============-->
-{{ old('office') }}
+@section('css')
+<link href="{{ asset('css/user-master/index.css') }}" rel="stylesheet">
+@endsection
+@section('content')
 <div class="container">
     <h3>ユーザーマスター　一覧</h3>
     <form method="get" action="{{ route('user.index') }}">
         <div class="form-group">
             <div class="row justify-content-start mx-auto my-2">
-                <label for="user_type" style="width: 100px;" class="text-md-left">ユーザー種別</label>
-                <select id="user_type" name="user_type" style="width: 100px;" class="form-select">
+                <label for="user_type" class="text-md-left label">ユーザー種別</label>
+                <select id="user_type" name="user_type" class="form-select">
                     <option value="" @if($filter_user_type_id === '') selected @endif>条件なし</option>
                     @foreach ($user_types as $user_type)
                         <option value="{{ $user_type->id }}" @if($filter_user_type_id == $user_type->id) selected @endif>{{ $user_type->alias }}</option>
@@ -20,25 +19,25 @@
                 <div class="col2"></div>
             </div>
             <div class="row justify-content-start mx-auto my-2">
-                <label for="office" style="width: 100px;" class="text-md-left">事業所</label>
-                <select id="office" name="office" style="width: 100px;" class="form-select">
+                <label for="office" class="text-md-left label">事業所</label>
+                <select id="office" name="office" class="form-select">
                     <option value="" @if($filter_office_id === '') selected @endif>条件なし</option>
                     @foreach ($offices as $office)
                         <option value="{{ $office->id }}" @if($filter_office_id == $office->id) selected @endif>{{ $office->office_name }}</option>
                     @endforeach
                 </select>
-                <input type="submit" style="margin-left: 50px;" class="text-md-right" value="絞り込み">
+                <input type="submit" class="text-md-right filter-button" value="絞り込み">
             </div>
         </div>
     </form>
-    <a href="{{ route('user.create') }}" type="button" class="btn btn-primary" style="margin: 10px;">新規作成</a>
+    <a href="{{ route('user.create') }}" type="button" class="btn btn-primary">新規作成</a>
     <table class="table table-striped table-bordered table-sm">
         <thead>
-            <tr style="font-size: 120%; background-color: MidnightBlue ; color: white;"><th style="width: 30%">氏名</th><th style="width: 25%">ユーザー種別</th><th style="width: 25%">事業所</th><th style="width: 20%">操作</th></tr>
+            <tr class="table-header"><th class="table-header-username">氏名</th><th class="table-header-usertype">ユーザー種別</th><th class="table-header-office">事業所</th><th class="table-header-action">操作</th></tr>
         </thead>
         <tbody>
             @foreach($users as $user)
-                <tr><td>{{ $user->name }}</td><td>{{ $user->user_type->alias }}</td><td>{{ $user->office->office_name }}</td><td><div style="text-align: center"><a href="{{ route('user.edit', $user->id) }}" type="button" class="btn btn-primary" style="margin-right: 13%; max-width: 30%; min-width: 40px; display: inline;">編集</a><form method="post" action="{{ route('user.destroy', $user->id) }}" style="display: inline;"> @csrf @method('DELETE') <input type="submit" value="削除" class="btn btn-danger" style="max-width: 30%; min-width: 40px; white-space: normal;"></form></td></tr>
+                <tr><td>{{ $user->name }}</td><td>{{ $user->user_type->alias }}</td><td>{{ $user->office->office_name }}</td><td><div class="table-body-action"><a href="{{ route('user.edit', $user->id) }}" type="button" class="btn btn-primary edit-button">編集</a><form method="post" action="{{ route('user.destroy', $user->id) }}" class="delete-form"> @csrf @method('DELETE') <input type="submit" value="削除" class="btn btn-danger delete-button"></form></td></tr>
             @endforeach
         </tbody>
     </table>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .css('resources/css/user-master/index.css', 'public/css/user-master')
+    .css('resources/css/user-master/create_and_edit.css', 'public/css/user-master');
