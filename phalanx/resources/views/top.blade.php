0d1ae1fd28963aa7021ac235cc6ace6099e937c1
appleumeda.nozato <appleumeda.nozato@gmail.com>
2021-11-05 11:33:31 +0900

    ログイン後、利用者に表示する画面の見た目を確認するために「user_page.blade.php」を作成しました。 コントローラーは「UserpageController」です。 「web.php」を上記のbladeファイルを表示できるように更新しました。

---
+++ phalanx/resources/views/top.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" 
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" 
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" 
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="common.css">
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/css/theme.default.min.css">
+  <link href="{{asset('/css/top.css?202111041116')}}" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+<nav class="navbar navbar-light bg-dark  ml-auto">
+  <b>タイトル</b>
+</nav><br>
+
+<button type="button" class="login btn btn-light">ログイン</button>
+
+<u class="sp"><center>私たちがサポートします</u><br><br><br><br>
+どの事業所が自分に合っているのか判定！<br><br>
+
+<button type="button" class="btn btn-success">適性診断</button><br><br>
+<button type="button" class="btn btn-primary">見学・体験申し込み</button>
+
+</center>
+
+</body>
