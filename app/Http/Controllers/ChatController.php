5fcbdb85a419cc015c6cb21282dc4c4dfeabb01c
k.k.apple0430 <k.k.apple0430@gmail.com>
2021-11-16 15:16:41 +0900

    細かいミスの修正

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -48,7 +48,7 @@ class ChatController extends Controller
         //ログイン中のユーザーが参加している部屋一覧を取得
         $joinRooms = ChatRoom::join("chat_room__user", "chat_rooms.id", "=", "chat_room__user.chat_room_id")
             ->where("chat_room__user.user_id", $user->id)->whereNull("chat_rooms.deleted_at")->whereNull("chat_room__user.deleted_at")
-                ->whereIn("chat_rooms.distinction_number", [1, 2, 4])->orderBy("chat_rooms.distinction_number")->get();
+                ->whereIn("chat_rooms.distinction_number", [1, 2, 4])->orderBy("chat_rooms.distinction_number")->get("chat_rooms.*");
 
         //事業所一覧を取得
         $offices = Office::whereNull("deleted_at")->orderBy("sort")->get();
