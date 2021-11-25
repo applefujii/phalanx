2c503056ca832e52ad49b96c153a2d0a58a51e09
望月 <fmochizukif@gmail.com>
2021-11-25 10:45:14 +0900

    チャット画面の見た目修正、送信時に最下までスクロールするよう修正

--- phalanx/resources/views/chat/show.blade.php
+++ phalanx/resources/views/chat/show.blade.php
@@ -27,7 +27,7 @@
 
         <div id="chat_footer" class="border col-md-8 d-flex align-items-center p-0">
             <div class="form-row col-12">
-                <div class="col-10">
+                <div class="col-11">
                     <textarea id="chat_text" name="chat_text" class="form-control chat_textarea @error('chat_text') is-invalid @enderror" rows="1" required="required">{{ old('chat_text') }}</textarea>
                     @error('chat_text')
                         <span class="invalid-feedback" role="alert">
@@ -35,8 +35,11 @@
                         </span>
                     @enderror
                 </div>
-                <div class="col-2">
-                    <button id="submit" class="btn btn-primary">送信</button>
+                <div class="col-1">
+                    <button id="submit" class="btn btn-primary"><svg xmlns="{{ asset('image/send-fill.svg') }}" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89.471-1.178-1.178.471L5.93 9.363l.338.215a.5.5 0 0 1 .154.154l.215.338 7.494-7.494Z"/>
+                      </svg>
+                    </button>
                 </div>
             </div>
         </div>
