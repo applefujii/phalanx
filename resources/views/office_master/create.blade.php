26f1e0142a24fbc3ac2f8505865797c498681c6e
appleumeda.nozato <appleumeda.nozato@gmail.com>
2021-11-26 11:27:09 +0900

    「OfficeController.php」にエラーメッセージを格納した配列を追加しました。 「office_master/create.blade.php」、「office_master/edit.blade.php」にエラーメッセージの処理を追加し、「事業所名」と「表示順」が未入力のとき、エラーメッセージを表示するようにしました。

--- resources/views/office_master/create.blade.php
+++ resources/views/office_master/create.blade.php
@@ -4,28 +4,40 @@
 <link href="{{ asset('css/user-master/create_and_edit.css') }}" rel="stylesheet">
 @endsection
 @section('content')
-<div class="container">
+<div class="container-md">
     <h3>事業所マスター　新規登録</h3>
     <form method="POST" action="{{ route('office.store') }}">
         @csrf
-
         <div class="form-group">
             <div class="row justify-content-start mx-auto my-4">
                 <div class="mx-4">
-                    <label for="text" class="text-md-left form-element">事業所名</label><br />
-                    <input type="text" id="text" class="form-control form-element @error('text') is-invalid @enderror" name="office_name" value="{{ old('office_name') }}" autofocus>
-                    @error('office_name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                    <label for="text" class="text-md-left form-element">事業所名</label><br>
+                    <input type="text" id="officename" name="office_name" class="form-control form-element" value="{{ old('office_name') }}"><br>
+                @if ($errors->has("office_name"))
+                   <!--  <div class="row mt-2">
+                        <ul class="pl-0" style="list-style: none"> -->
+                            @foreach ($errors->get("office_name") as $error)
+                            <li class="text-danger">{{ $error }}</li> 
+                            @endforeach
+                        <!-- </ul>
+                    </div> -->
+                @endif
                 </div>
                 <div class="mx-4">
-                    <label for="text" class="text-md-left form-element">表示順</label><br />
-                    <input type="number" id="number" class="form-control form-element" name="sort">
+                    <label for="text" class="text-md-left form-element">表示順</label><br>
+                    <input type="number" id="sort" name="sort" class="form-control form-element" value="{{ old('sort') }}"><br>
+                @if ($errors->has("sort"))
+                    <!-- <div class="row mt-2"> -->
+                        <!-- <ul class="pl-0" style="list-style: none"> -->
+                                @foreach ($errors->get("sort") as $error)
+                                    <li class="text-danger">{{ $error }}</li>
+                                @endforeach
+                        <!-- </ul> -->
+                    <!-- </div> -->
+                @endif
                 </div>
             </div>
-
+        </div>
         <input type="submit" class="btn btn-primary" value="登録"><a href="{{ route('office.index') }}" class="btn btn-secondary back-button">戻る</a>
     </form>
 </div>
