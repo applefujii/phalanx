3857b3f1443ed0d38c2b6838329c34566d193d8f
k.k.apple0430 <k.k.apple0430@gmail.com>
2021-11-29 14:21:02 +0900

    細かい調整

--- resources/views/chat/sidebar.blade.php
+++ resources/views/chat/sidebar.blade.php
@@ -324,7 +324,7 @@ if(isset($chat_room)) {
 
 @section('script')
 {{-- jQuery読み込み --}}
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 
 <script>
     $(function() {
