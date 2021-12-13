5c16a7d5fe99d7b965788abab74bc2d049e99b96
望月 <fmochizukif@gmail.com>
2021-12-13 13:24:19 +0900

    チャット画面の不具合修正

--- resources/views/chat/sidebar_right.blade.php
+++ resources/views/chat/sidebar_right.blade.php
@@ -22,7 +22,7 @@
         @if ($user_type->id == 3 && $chat_room->users->where('user_type_id', 3)->where('office_id', $office->id)->count())
             <div>
                 <a data-toggle="collapse" href="#trial">
-                    <h5 class="col-12 pt-3 text-dark font-weight-bold">{{ $user_type->alias }} ({{ $chat_room->users->where('user_type_id', $user_type->id)->where('office_id', $office->id)->count() }})</h5>
+                    <h5 class="col-12 pt-3 text-dark font-weight-bold">{{ $user_type->alias }} ({{ $chat_room->users->where('user_type_id', $user_type->id)->count() }})</h5>
                 </a>
                 <ul @if($user_type->id === 1)id="staff-{{ $office->id }}" @elseif ($user_type->id === 2) id="user-{{ $office->id }}" @else id="trial"@endif class="col-12 pt-1 collapse show">
                     @foreach ($chat_room->users as $user)
