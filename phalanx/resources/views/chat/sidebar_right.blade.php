426483f8426997211a3aca16ae11247d30808275
望月 <fmochizukif@gmail.com>
2021-12-13 10:25:20 +0900

    チャット画面右サイドバーに修正不備があったため修正

--- phalanx/resources/views/chat/sidebar_right.blade.php
+++ phalanx/resources/views/chat/sidebar_right.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         @foreach ($offices as $office)
             <div>
-                <a data-toggle="collapse" href="#staff-{{ $office->id }}">
+                <a data-toggle="collapse" @if($user_type->id === 1)href="#staff-{{ $office->id }}" @else href="#user-{{ $office->id }}"@endif>
                     <h5 class="col-12 pt-3 {{ $user_type->type_color }} font-weight-bold">{{ $office->office_name }}{{ $user_type->alias }} ({{ $chat_room->users->where('user_type_id', $user_type->id)->where('office_id', $office->id)->count() }})</h5>
                 </a>
-                <ul id="staff-{{ $office->id }}" class="col-12 pt-1 collapse show">
+                <ul @if($user_type->id === 1)id="staff-{{ $office->id }}" @else id="user-{{ $office->id }}"@endif class="col-12 pt-1 collapse show">
                     @foreach ($chat_room->users as $user)
                         @if ($user->user_type_id == $user_type->id && $user->office_id == $office->id)
                             <li class="@if ($user->id == Auth::user()->id) text-primary @elseif($user->user_type_id == 1) text-danger @else text-success @endif">{{ $user->name }}</li>
