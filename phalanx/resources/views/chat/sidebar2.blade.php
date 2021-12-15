94b17100bb709250f0f4176589b07c439500b267
望月 <fmochizukif@gmail.com>
2021-12-15 11:38:58 +0900

    チャット画面で未読ルームの色変更が機能していなかったので修正

--- phalanx/resources/views/chat/sidebar2.blade.php
+++ phalanx/resources/views/chat/sidebar2.blade.php
@@ -116,6 +116,10 @@
             }
         });
 
+        //未読テキストがあるチャットルームのリンクの色を変更
+        $.map(@json($unreadId), (val, index) => {
+                $(`#chat_room_${val}`).addClass("text-danger");
+            });
     });
 
 </script>
