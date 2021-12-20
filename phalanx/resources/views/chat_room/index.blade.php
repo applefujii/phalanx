cbd11323d8b8fb36608ffbfdb81e370ea665cc93
k.k.apple0430 <k.k.apple0430@gmail.com>
2021-12-20 14:58:59 +0900

    細かい調整

--- phalanx/resources/views/chat_room/index.blade.php
+++ phalanx/resources/views/chat_room/index.blade.php
@@ -31,19 +31,15 @@
                             @endif
                         </td>
                         <td class="align-middle">
-                            <div class="row gx-2">
-                                <div class="col">
-                                    <form action="{{ route('chat_room.edit', $chatRoom->id) }}" method="GET">
-                                        <button class="btn btn-sm btn-primary" type="submit">編集</button>
-                                    </form>
-                                </div>
-                                <div class="col">
-                                    <form action="{{ route('chat_room.destroy', $chatRoom->id) }}" method="POST">
-                                        @csrf
-                                        @method("DELETE")
-                                        <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('本当に削除しますか？')">削除</button>
-                                    </form>
-                                </div>
+                            <div class="table-body-action">
+                                <form action="{{ route('chat_room.edit', $chatRoom->id) }}" method="GET">
+                                    <button class="btn btn-sm btn-primary" type="submit">編集</button>
+                                </form>
+                                <form action="{{ route('chat_room.destroy', $chatRoom->id) }}" method="POST">
+                                    @csrf
+                                    @method("DELETE")
+                                    <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('本当に削除しますか？')">削除</button>
+                                </form>
                             </div>
                         </td>
                     </tr>
