9c628abee4b9607604b31628b5a4abaa906ca7ce
望月 <fmochizukif@gmail.com>
2021-12-20 11:39:53 +0900

    ユーザーページ画面で体験新規申し込みがあったときの表示方法修正

--- phalanx/resources/views/user_page.blade.php
+++ phalanx/resources/views/user_page.blade.php
@@ -12,6 +12,9 @@
           <button class="btn btn-primary" onclick="location.href='{{ route("chat_room.index") }}'">チャットルーム管理</button>
           <button class="btn btn-primary" onclick="location.href='{{ route("notification.index") }}'">通知管理</button>
           <button class="btn @if ($new_trial_applications) btn-warning @else btn-primary @endif" onclick="location.href='{{ route("trial_application_manage.index", ["office_id" => Auth::user()->office_id]) }}'">体験・見学申込管理</button>
+          @if ($new_trial_applications)
+              <span class="text-danger font-weight-bold">新規申し込みがあります</span>
+          @endif
           <button class="btn btn-primary" onclick="location.href='{{ route("aptitude_question_manage.index") }}'">適性診断質問管理</button>
         @else
           <button class="btn btn-secondary" onclick="location.href='{{ route('top') }}'">トップに戻る</button>
