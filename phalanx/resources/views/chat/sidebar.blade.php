3177b25a8bb5377d3c27434ce5b04fcf262b7da4
望月 <fmochizukif@gmail.com>
2021-12-22 11:12:04 +0900

    チャット画面で、チャットルームに新着があるときに文字色だけでなくマークも表示するよう修正

--- phalanx/resources/views/chat/sidebar.blade.php
+++ phalanx/resources/views/chat/sidebar.blade.php
@@ -98,7 +98,8 @@
 
             //未読テキストがあるチャットルームのリンクの色を変更
             $.map(@json($unreadId), (val, index) => {
-                $(`#chat_room_${val}`).addClass("text-danger");
+                $(`.chat_room_${val}`).addClass("text-danger");
+                $(`.chat_room_${val}`).children('span').removeClass('d-none');
             });
         });
     </script>
