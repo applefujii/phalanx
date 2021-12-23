b3fc97d2dc9c16f6aebcd192d8320ba378393cb5
k.k.apple0430 <k.k.apple0430@gmail.com>
2021-12-23 12:48:41 +0900

    適性診断削除時のポップアップのIDの空欄埋め

--- resources/views/aptitude_question_manage/index.blade.php
+++ resources/views/aptitude_question_manage/index.blade.php
@@ -29,7 +29,7 @@
                 @method('DELETE')
                 @csrf
                 <button class="btn btn-sm btn-danger" type="submit"
-                    onclick="return confirm('削除しますか\nID: \n質問文: {{ $aptitude_question->question }}\n表示順: {{ $aptitude_question->sort }}\nアップル点数: {{ $aptitude_question->score_apple }}\nミント点数: {{ $aptitude_question->score_mint }}\nメープル点数: {{ $aptitude_question->score_maple }}')">
+                    onclick="return confirm('削除しますか\nID: {{ $aptitude_question->id }}\n質問文: {{ $aptitude_question->question }}\n表示順: {{ $aptitude_question->sort }}\nアップル点数: {{ $aptitude_question->score_apple }}\nミント点数: {{ $aptitude_question->score_mint }}\nメープル点数: {{ $aptitude_question->score_maple }}')">
                 削除</button>
             </form>
         </div>
