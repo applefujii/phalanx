68f39c14b739ede87f533d41abf31fd8a734b417
望月 <fmochizukif@gmail.com>
2021-12-23 13:11:27 +0900

    公式サイトのリンクを開いたときに別タブで開くように修正

--- phalanx/resources/views/aptitude_question_form/result/common.blade.php
+++ phalanx/resources/views/aptitude_question_form/result/common.blade.php
@@ -12,7 +12,7 @@
 
     <img @yield('logo') alt="@yield('office_name')" class="logo">
     
-    <p><a class="btn btn-primary mt-4 col-md-2" role="button" @yield('official')>公式サイト</a></p>
+    <p><a class="btn btn-primary mt-4 col-md-2" role="button" @yield('official') target="_blank" rel="noopener noreferrer">公式サイト</a></p>
     <p><a class="btn btn-primary col-md-2" role="button" @yield('trial_form')>体験・見学申し込み</a></p>
     <p><a class="btn btn-secondary col-md-2" role="button" href="{{ route('top') }}">トップに戻る</a></p>
 </div>
