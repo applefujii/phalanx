6414150fc4712a90101a64ab3ed0b240747939c5
望月 <fmochizukif@gmail.com>
2021-12-28 10:01:22 +0900

    適性診断フォームのレイアウト修正

--- resources/views/aptitude_question_form/index.blade.php
+++ resources/views/aptitude_question_form/index.blade.php
@@ -10,59 +10,52 @@
     <div class="container">
         <div class="col-lg-12">
             <h3 class="">適性診断</h3>
-        </div>
-        @if ($errors->any())
-            <div class="col-lg-12">
-                <div class="alert alert-danger">
-                    <span>未回答の質問があります。</span>
-                </div>
-            </div>
-        @endif
-        <form action="{{ route('aptitude_question_form.calculate') }}" method="post">
-            @csrf
-            <div class="col-lg-12">
-            @foreach ($aptitude_questions as $aptitude_question)
-                <input type="hidden" name="aptitude_questions[{{ $aptitude_question->id }}][id]" value="{{ $aptitude_question->id }}">
-                <input type="hidden" name="aptitude_questions[{{ $aptitude_question->id }}][score_apple]" value="{{ $aptitude_question->score_apple ?? 0 }}">
-                <input type="hidden" name="aptitude_questions[{{ $aptitude_question->id }}][score_mint]" value="{{ $aptitude_question->score_mint ?? 0 }}">
-                <input type="hidden" name="aptitude_questions[{{ $aptitude_question->id }}][score_maple]" value="{{ $aptitude_question->score_maple ?? 0 }}">
-                <div class="aptitude_question row">
-                    <div class="question col-lg-7 p-3">
-                        {{ $aptitude_question->question }}
+            @if ($errors->any())
+                <div class="col-lg-12">
+                    <div class="alert alert-danger">
+                        <span>未回答の質問があります。</span>
                     </div>
-                    <div class="answer col-lg-5 row p-3">
-                        @foreach (config('const.option') as $option_key => $option_value)
-                            <div class="radio col-lg-auto">
-                                <div class="form-check">
-                                    <input id="{{ $option_key }}[{{ $aptitude_question->id }}]"
-                                        class="form-check-input" type="radio"
-                                        name="aptitude_questions[{{ $aptitude_question->id }}][answer]"
-                                        value="{{ $option_value }}" @if (!is_null(old("aptitude_questions.$aptitude_question->id.answer")) && (int) old("aptitude_questions.$aptitude_question->id.answer") === $option_value) checked @endif>
-                                    <label class="form-check-label"
-                                        for="{{ $option_key }}[{{ $aptitude_question->id }}]">{{ config('const.option_japanese')[$option_key] }}</label>
+                </div>
+            @endif
+            <form action="{{ route('aptitude_question_form.calculate') }}" method="post">
+                @csrf
+                @foreach ($aptitude_questions as $aptitude_question)
+                    <input type="hidden" name="aptitude_questions[{{ $aptitude_question->id }}][id]" value="{{ $aptitude_question->id }}">
+                    <input type="hidden" name="aptitude_questions[{{ $aptitude_question->id }}][score_apple]" value="{{ $aptitude_question->score_apple ?? 0 }}">
+                    <input type="hidden" name="aptitude_questions[{{ $aptitude_question->id }}][score_mint]" value="{{ $aptitude_question->score_mint ?? 0 }}">
+                    <input type="hidden" name="aptitude_questions[{{ $aptitude_question->id }}][score_maple]" value="{{ $aptitude_question->score_maple ?? 0 }}">
+                    <div class="aptitude_question row">
+                        <div class="question col-lg-7 p-3">
+                            {{ $aptitude_question->question }}
+                        </div>
+                        <div class="answer col-lg-5 row p-3 d-flex justify-content-end">
+                            @foreach (config('const.option') as $option_key => $option_value)
+                                <div class="radio col-lg-auto">
+                                    <div class="form-check">
+                                        <input id="{{ $option_key }}[{{ $aptitude_question->id }}]"
+                                            class="form-check-input" type="radio"
+                                            name="aptitude_questions[{{ $aptitude_question->id }}][answer]"
+                                            value="{{ $option_value }}" @if (!is_null(old("aptitude_questions.$aptitude_question->id.answer")) && (int) old("aptitude_questions.$aptitude_question->id.answer") === $option_value) checked @endif>
+                                        <label class="form-check-label"
+                                            for="{{ $option_key }}[{{ $aptitude_question->id }}]">{{ config('const.option_japanese')[$option_key] }}</label>
+                                    </div>
                                 </div>
-                            </div>
-                        @endforeach
-                        @error("aptitude_questions.$aptitude_question->id.answer")
-                            <p class="text-danger pl-3" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </p>
-                        @enderror
+                            @endforeach
+                            @error("aptitude_questions.$aptitude_question->id.answer")
+                                <p class="text-danger pl-3" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </p>
+                            @enderror
+                        </div>
                     </div>
-                </div>
-            @endforeach
-            </div>
-
-            <div class="form-group col-lg-12">
+                @endforeach
                 <div class="text-center mt-4">
                     <button class="btn btn-primary col-lg-3" type="submit">結果を見る</button>
                 </div>
-            </div>
-        </form>
-        <div class="col-lg-12">
-            <div class="text-center">
-                <p><a class="btn btn-secondary col-lg-3" role="button" href="{{ route('top') }}">トップに戻る</a></p>
-            </div>
+            </form>
+                <div class="text-center mt-3">
+                    <p><a class="btn btn-secondary col-lg-3" role="button" href="{{ route('top') }}">トップに戻る</a></p>
+                </div>
         </div>
     </div>
 @endsection
