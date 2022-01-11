8e74ddc10effac67ba73505da94a23bdb1816d40
apple-fujii <apple.fujii333@gmail.com>
2022-01-11 11:30:54 +0900

    通知対象の事業所選択で体験者を選択した時の表示が正しくなるように修正。

--- phalanx/app/Http/Controllers/UserpageController.php
+++ phalanx/app/Http/Controllers/UserpageController.php
@@ -49,7 +49,11 @@ class UserpageController extends Controller
             $n__u->where('user_id', '=', Auth::id());
         })->orWhere(function($query) {
             $query->whereHas('notification__office', function($n__o) {
-                $n__o->where('office_id', '=', Auth::user()->office_id);
+                    if( Auth::user()->user_type_id == 3 ) {
+                    $n__o->where('office_id', '=', 0);
+                } else {
+                    $n__o->where('office_id', '=', Auth::user()->office_id);
+                }
             });
         })->orderBy('start_at', 'asc')->orderBy('end_at', 'asc')->get();
         $unsorted_notifications_groups = $notifications->mapToGroups(function ($notification, $key) {
