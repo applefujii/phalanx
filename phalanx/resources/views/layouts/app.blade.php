6da451ba310a32246716f80890e71956b3260aba
k.k.apple0430 <k.k.apple0430@gmail.com>
2022-03-08 13:05:17 +0900

    レイアウトの崩れの修正

--- phalanx/resources/views/layouts/app.blade.php
+++ phalanx/resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-dark shadow-sm">
             <div class="container-fluid">
-                <a class="navbar-brand text-light" href="{{ route('user_page') }}">
+                <a class="navbar-brand text-light d-inline-block" id="navbarTitle" href="{{ route('user_page') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
